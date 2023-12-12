343a6f5d376920eabb37dc8206e8c56232df9a4a
khoa1812 <khoatrinh2909@gmail.com>
2023-12-12 14:00:49 +0700

    1

---
+++ Main.java
@@ -0,0 +1,29 @@
+package Watch;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[100000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100000);
+        }
+
+        StopWatch sw = new StopWatch();
+
+        sw.start();
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+        sw.stop();
+
+        System.out.println("Thời gian thực thi: " + sw.getElapsedTime() + " milliseconds");
+    }
+}
+
